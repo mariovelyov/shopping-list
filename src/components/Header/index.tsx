2fc8d88f6f6072df2dec4dc9868f241bda8c2706
mariovelyov <83953706+mariovelyov@users.noreply.github.com>
2023-06-21 15:07:33 +0300

    logo import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import classes from "./index.module.css";
+import logo from "../../assets/app_logo.png";
 
 function Header() {
   return (
     <header className={classes.header}>
-      <img
-        src="/images/app_logo.png"
-        alt="Shopping List Logo"
-        className={classes.image}
-      />
+      <img src={logo} alt="Shopping List Logo" className={classes.image} />
       <h1 className={classes.h1}>Shopping List</h1>
     </header>
   );
